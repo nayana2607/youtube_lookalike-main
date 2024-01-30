468628df50f301e797a6dfd3df8fecd25ba1fed2
Nayana_Bhovi <jforme776@gmail.com>
2024-01-30 11:33:39 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <>
+     <div><h1 className='bg-red-500'> My Youtube</h1></div>
+     </>
+    </div>
+  );
+}
+
+export default App;
